6d58bb5a7b195feedada34c23441043d2f5baf46
Peetr0 <derdour.badreddine@gmail.com>
2024-04-12 23:23:44 +0100

    sup

--- lib/features/sign_up/views/widgets/sign_up_view_body.dart
+++ lib/features/sign_up/views/widgets/sign_up_view_body.dart
@@ -1,17 +1,30 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/widgets.dart';
 class SignUpViewbody extends StatelessWidget {
   const SignUpViewbody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Center(
-          child: Text('registration page df'),
-        )
-      ],
+    return  SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+           Container(
+            width: double.infinity,
+            height:500,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+              image: AssetImage("assets/images/bg2.png"),
+              fit: BoxFit.fill)),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal:24),
+              ),
+            ),
+
+            )
+        ],
+      ),
     );
   }
 }
